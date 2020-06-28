450f3e4a1709b1686bea4ea96a759a630e81687f
Lukasur <51718601+Lukasur@users.noreply.github.com>
2020-06-28 17:09:14 +0200

    2

--- fps/Assets/Scripts/Move.cs
+++ fps/Assets/Scripts/Move.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+	public CharacterController charContr;
+	
+	private float xDir;
+	private float yDir;
+	private float zDir;
+	
+	private Vector3 moveDirection;
+	
+	public float speed = 10f;
+	public float jump = 1f;
+	public float groundDist = 0.2f;
+	public LayerMask groundLayer;
+	
+	public Transform grandCheck;
+	private bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +28,26 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(grandCheck.position, groundDist, groundLayer);
+		
+		if(isGrounded && yDir <0)
+		{
+			yDir =-2f;
+		}
+		
+		xDir = Input.GetAxis("Horizontal");
+		zDir = Input.GetAxis("Vertical");
+		
+		yDir += Physics.gravity.y * Time.deltaTime;
+		
+		moveDirection = transform.right * xDir * speed +transform.forward * zDir * speed + transform.up * yDir;
+		charContr.Move(moveDirection * Time.deltaTime);
+		
+		if(Input.GetButtonDown("Jump") && isGrounded)
+		{
+			yDir = Mathf.Sqrt(jump * -2f * Physics.gravity.y);
+		}
+		
+		
     }
 }
